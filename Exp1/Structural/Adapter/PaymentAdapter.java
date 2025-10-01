d4fb5c13b50b8a7148ff59bb002925c87c207531
abi1104 <144511921+abi1104@users.noreply.github.com>
2025-10-01 22:18:16 +0530

    Update PaymentAdapter.java

--- Exp1/Structural/Adapter/PaymentAdapter.java
+++ Exp1/Structural/Adapter/PaymentAdapter.java
@@ -6,10 +6,7 @@ public class PaymentAdapter implements NewPaymentGateway {
     public PaymentAdapter(OldPaymentSystem oldSystem) {
         this.oldSystem = oldSystem;
     }
-
-    @Override
     public void makePayment(double amount) {
-        // Convert double to int for old system
         int roundedAmount = (int) amount;
         oldSystem.payAmount(roundedAmount);
     }
